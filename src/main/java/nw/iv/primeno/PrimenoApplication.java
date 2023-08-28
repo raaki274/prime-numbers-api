8515bdf094d7195765f3f508494a1dee21155b64
Radhakrishnan Balakrishnan <raaki274@Radhakrishnans-MacBook-Pro-3.local>
2023-08-28 12:45:31 +0100

    boiler plate

---
+++ src/main/java/nw/iv/primeno/PrimenoApplication.java
@@ -0,0 +1,13 @@
+package nw.iv.primeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nw/iv/primeno/PrimenoApplicationTests.java
@@ -0,0 +1,13 @@
+package nw.iv.primeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
